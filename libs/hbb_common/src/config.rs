44a4ba697d5a2596ee10e27859db3f3a61e08870
winxsj <53484755+winxsj@users.noreply.github.com>
2024-08-23 15:46:31 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.lcyun.cc"];
-pub const PUBLIC_RS_PUB_KEY: &str = "VODPpIWneSNsVnjsiBuFjKnZZjkGGMCentzgQUu+2ms=";
+pub const PUBLIC_RS_PUB_KEY: &str = "qq4950311";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
